c8e8247d541520dd3d6b3d99881bf21a19eb235f
itsparasbisht <iamparasbisht@gmail.com>
2022-10-26 19:34:28 +0530

    get title image price object

--- parser.js
+++ parser.js
@@ -9,19 +9,37 @@ async function scrapPrice() {
     const page = await browser.newPage();
     await page.goto(URL);
 
-    let data = await page.evaluate(() => {
+    // get item title
+    let title = await page.evaluate(() => {
+      let item = document.querySelector("#productTitle");
+      return item.innerText;
+    });
+
+    // get item price
+    let price = await page.evaluate(() => {
       let item = document.querySelector(".a-price-whole");
       return item.innerText.replace(/[.,]/g, "");
     });
+
+    // get item image
+    let image = await page.evaluate(() => {
+      let item = document.querySelector("#landingImage");
+      return item.getAttribute("src");
+    });
+
     await browser.close();
 
-    return parseInt(data);
+    return {
+      title,
+      image,
+      price: parseInt(price),
+    };
   } catch (error) {
     console.error(error);
   }
 }
 
-const price = scrapPrice();
-price.then((data) => {
+const data = scrapPrice();
+data.then((data) => {
   console.log(data);
 });
