92b7df6e64732118205ba9d16752cf7e2a573fc1
itsparasbisht <iamparasbisht@gmail.com>
2022-10-27 19:21:35 +0530

    code formatting

---
+++ frontend/components/Main.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "../styles/main.module.css";
+import Image from "next/image";
+import rupeeIcon from "../assets/rupee-indian.png";
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <nav className={styles.navbar}>
+        <Image src={rupeeIcon} alt="" />
+        <h3>Amazon Price Tracker</h3>
+      </nav>
+
+      <form className={styles.getItemForm}>
+        <input type="url" name="url" id="url" />
+        <button type="button">get item</button>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,20 +1,9 @@
-import Image from "next/image";
-import rupeeIcon from "../assets/rupee-indian.png";
-
-console.log(rupeeIcon);
+import Main from "../components/Main";
 
 export default function Home() {
   return (
     <div>
-      <nav>
-        <Image src={rupeeIcon} alt="" />
-        <h3>Amazon Price Tracker</h3>
-      </nav>
-
-      <form>
-        <input type="url" name="url" id="url" />
-        <button type="button">get item</button>
-      </form>
+      <Main />
     </div>
   );
 }
