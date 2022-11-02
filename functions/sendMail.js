549d650f48f180c8e25ec5a71ed48373109463da
itsparasbisht <iamparasbisht@gmail.com>
2022-11-02 20:01:36 +0530

    send mail function

---
+++ functions/sendMail.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const appPass = process.env.GMAIL_PASSWORD;
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  auth: {
+    user: "parash@sirpi.io",
+    pass: appPass,
+  },
+});
+
+const sendMail = async () => {
+  let info = await transporter.sendMail({
+    from: "parash@sirpi.io",
+    to: "parasbisht.web@gmail.com",
+    subject: "Testing the dev",
+    html: "<dvi><b>Hello there...</b><br /> <img src='https://i.quotev.com/l7jmhx3sppoa.jpg' /></div>",
+  });
+
+  if (info.messageId) {
+    console.log("Mail sent, id: ", info.messageId);
+  }
+};
+
+sendMail().catch((error) => {
+  console.log(
+    "------------------------Failed to send mail----------------------------"
+  );
+  console.log(error);
+});
+
+module.exports = sendMail;
