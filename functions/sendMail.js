7add3aebdbb5e3b1b5e30f75fc67b9c81ead4023
itsparasbisht <iamparasbisht@gmail.com>
2022-11-04 20:23:00 +0530

    mail function

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -5,6 +5,15 @@ const appPass = process.env.GMAIL_PASSWORD;
 const fs = require("fs");
 const mailBody = fs.readFileSync(`${__dirname}/mailBody.html`).toString();
 
+const title =
+  "CHROES® Men's Fall Winter Office Single Breasted Long Dress Wool Coat Overcoat";
+const imageUrl = "https://m.media-amazon.com/images/I/41ZvSZbAKRL._UL1001_.jpg";
+const price = "5000";
+const productUrl =
+  "https://www.amazon.in/CHROES%C2%AE-Winter-Office-Breasted-Overcoat/dp/B0B9WWQRR2";
+const mailTemplate = require("./mailBody");
+const mailContent = mailTemplate(title, imageUrl, price, productUrl);
+
 let transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
@@ -19,7 +28,7 @@ const sendMail = async () => {
     from: "parash@sirpi.io",
     to: "parasbisht.web@gmail.com",
     subject: "Price Update: Amazon Price Tracker",
-    html: mailBody,
+    html: mailContent,
   });
 
   if (info.messageId) {
